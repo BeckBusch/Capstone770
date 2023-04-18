b308f2db65321c8b60a3dfd8cbc29ca5020f582e
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-04-18 16:58:12 +1200

    created navigation bar component

--- spca-web/src/pages/login-page.js
+++ spca-web/src/pages/login-page.js
@@ -1,14 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom";
 import "./login-page.css";
-import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 function LoginPage() {
     return (
         <div className="login-page">
             <div className="align-right">
                 <div className="login-container">
-                    <img className="spca-logo" src={require("./images/spca-logo.png")} alt="SPCA Logo" />
+                    <img className="spca-logo" src={require("../images/spca-logo.png")} alt="SPCA Logo" />
 
                     <form>
                         <div>
@@ -18,18 +17,14 @@ function LoginPage() {
                             <input type="password" placeholder="Password" />
                         </div>
                         <div>
-                            {/* <button type="submit" id="logInBtn" onSubmit={LogIn()}>Log In</button> */}
-
-
                             <Link to="/dashboard">
-                                <button type="submit" id="logInBtn">Log In</button>
+                                <button type="submit" id="logInBtn" className="login-btn">Log In</button>
                             </Link>
-
                         </div>
                     </form>
 
                     <div>
-                        <p> Don"t have an account? <Link to="/sign-in">Sign In</Link> </p>
+                        <p className="sign-up-msg"> Don"t have an account? <Link to="/sign-in">Sign In</Link> </p>
                     </div>
                 </div>
             </div>
