3ac27d00f0bfdd7dbae693c7e2b20938ae3d290b
jjun273 <jjun273@aucklanduni.ac.nz>
2023-04-19 01:36:49 +1200

    Created sign-up-confirm-page javascript and css

---
+++ spca-web/src/pages/sign-up-confirm-page.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./sign-up-confirm-page.css";
+
+function SignUpConfirmPage() {
+    return (
+        <div className="sign-up-confirm-page">
+            <div className="nav-bar">
+                <div className="col col-left">
+                    <img className="navbar-logo" src={require("../images/dashboard-logo.png")} alt="SPCA Logo" />
+                    {/* <Link to="/sign-in">Sign In</Link> */}
+                </div>
+            </div>    
+            <div className="align-center">
+                <div className="sign-up-confirm-container">
+                    <p>Your request has been sent to an admin for approval.</p>
+                    <p>A confirmation email will be sent once your request has been approved.</p>
+                    <p className="back-to-login-msg"><Link to="/">Back to Log In Page</Link> </p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignUpConfirmPage;
