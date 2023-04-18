871ec7fae6763e4b08d674ed052c713073164c9f
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-04-18 14:08:31 +1200

    linked to dashboard but no authentication

--- spca-web/src/pages/login-page.js
+++ spca-web/src/pages/login-page.js
@@ -1,34 +1,36 @@
-import React from 'react'
+import React from "react"
 import { Link } from "react-router-dom";
-import './login-page.css';
+import "./login-page.css";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 function LoginPage() {
     return (
-        <div className='login-page'>
-
-            <div className='login-justify'>
-                <div className='login-container'>
-
-
-                    <img className="spca-logo" src={require('./images/spca-logo.png')} alt='SPCA Logo' />
+        <div className="login-page">
+            <div className="align-right">
+                <div className="login-container">
+                    <img className="spca-logo" src={require("./images/spca-logo.png")} alt="SPCA Logo" />
 
                     <form>
                         <div>
-                            <input type="text" placeholder='Username' />
+                            <input type="text" placeholder="Username" />
                         </div>
                         <div>
-                            <input type="password" placeholder='Password' />
+                            <input type="password" placeholder="Password" />
                         </div>
                         <div>
-                            <button type="submit">Log In</button>
+                            {/* <button type="submit" id="logInBtn" onSubmit={LogIn()}>Log In</button> */}
+
+
+                            <Link to="/dashboard">
+                                <button type="submit" id="logInBtn">Log In</button>
+                            </Link>
+
                         </div>
                     </form>
 
                     <div>
-                        <p> Don't have an account? <Link to="/sign-in">Sign In</Link> </p>
+                        <p> Don"t have an account? <Link to="/sign-in">Sign In</Link> </p>
                     </div>
-
-
                 </div>
             </div>
         </div>
