403c286817549d3a7814d92087fa079a99cb0806
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-04-24 20:24:41 +1200

    made dashbaord card and created mock json file

---
+++ spca-web/src/components/dashboard-card.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./dashboard-card.css";
+import { Link } from "react-router-dom";
+
+function DashboardCard() {
+    return (            
+            
+        <div className="card-section">
+            
+            <div className="dashboard-card">
+            </div>
+
+        </div>
+
+    );
+}
+
+export default DashboardCard;
--- spca-web/src/pages/dashboard-page.js
+++ spca-web/src/pages/dashboard-page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./dashboard.css";
 import NavBar from "../components/nav-bar";
+import DashboardCard from "../components/dashboard-card";
 
 function DashboardPage() {
     return (
@@ -8,6 +9,21 @@ function DashboardPage() {
 
         <div className="dashboard-page">
             <NavBar/>
+
+            <div className="search-div">
+                <input type="text" placeholder="Search by name, breed etc." className="dashboard-search"/>
+                <img className="search-btn" src={require("../images/search-icon.png")} alt="Search" />
+            </div>
+
+
+            <div className="dashboard-panel">
+            <DashboardCard/>
+            <DashboardCard/>
+            <DashboardCard/>
+            <DashboardCard/>
+            </div>
+
+
 
         </div>
 
