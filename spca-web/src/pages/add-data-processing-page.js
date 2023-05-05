9cccb714f9c9455a0b39b2b3911cb57a4c77d903
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-05 21:45:25 +1200

    Created add data processing page

---
+++ spca-web/src/pages/add-data-processing-page.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import NavBar from "../components/nav-bar";
+import "./add-data-processing-page.css";
+
+function AddDataProcessingPage() {
+    return (
+        <div className="add-dog-processing-page">
+                <NavBar/>
+        </div>
+    )
+}
+
+export default AddDataProcessingPage;
