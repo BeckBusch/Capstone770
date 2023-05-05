5fc21b0f234abfd7b2ea132699dcc08b7f0b5fc9
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-06 02:18:57 +1200

    Created add data processing page

--- spca-web/src/pages/add-data-processing-page.js
+++ spca-web/src/pages/add-data-processing-page.js
@@ -7,6 +7,21 @@ function AddDataProcessingPage() {
     return (
         <div className="add-dog-processing-page">
                 <NavBar/>
+                <div className="circle">
+                    <div className="circle-container">
+                        <div className="footprint-container">
+                            <img className="dog-footprint-img one" src={require("../images/dog-footprint-image.png")} alt="Dog Footprint Image" />
+                            <img className="dog-footprint-img two" src={require("../images/dog-footprint-image.png")} alt="Dog Footprint Image" />
+                            <img className="dog-footprint-img three" src={require("../images/dog-footprint-image.png")} alt="Dog Footprint Image" />
+                        </div>
+                        <div className="text-container">
+                            <p className="processing-text">Processing...</p>
+                        </div>
+                    </div>
+                </div>
+                <Link to="/dog-detail">
+                    <button type="submit" id="cancelBtn" className="next-btn">Next</button>
+                </Link>
         </div>
     )
 }
