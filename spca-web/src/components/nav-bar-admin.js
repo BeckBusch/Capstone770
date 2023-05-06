6fac18cffe109254aaa5505b141325219dc5eb67
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-06 16:17:02 +1200

    Created nav bar admin

---
+++ spca-web/src/components/nav-bar-admin.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./nav-bar.css";
+import { Link } from "react-router-dom";
+
+function NavBarAdmin() {
+    return (
+        <div className="nav-bar">
+
+            <div className="col col-left">
+                <img className="navbar-logo" src={require("../images/dashboard-logo.png")} alt="SPCA Logo" />
+                {/* <Link to="/sign-in">Sign In</Link> */}
+            </div>
+
+            <div className="col col-mid">
+                <Link to="/dashboard">
+                    <img className="navbar-icon" src={require("../images/dashboard-icon.png")} alt="Dashboard" />
+                    <button type="submit" id="logInBtn" className="navbar-link">Dashboard</button>
+                </Link>
+            </div>
+
+            <div className="col col-mid">
+                <Link to="/chat">
+                    <img className="navbar-icon" src={require("../images/chat-icon.png")} alt="Dashboard" />
+                    <button type="submit" id="logInBtn" className="navbar-link">Chat</button>
+                </Link>
+            </div>
+
+            <div className="col col-mid">
+                <Link to="/manage-users">
+                    <img className="navbar-icon" src={require("../images/settings-icon.png")} alt="Dashboard" />
+                    <button type="submit" id="logInBtn" className="navbar-link">Manage Users</button>
+                </Link>
+            </div>
+
+            <div className="col col-right">
+                <Link to="/my-account">
+                    <button type="submit" id="logInBtn" className="navbar-link">My Account</button>
+                    <img className="navbar-icon" src={require("../images/my-account-icon.png")} alt="Dashboard" />
+                </Link>
+            </div>
+
+        </div>
+    );
+}
+
+export default NavBarAdmin;
