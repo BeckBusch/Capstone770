701d278eec657a77a833e8666bb29b4aae85e756
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-14 22:10:50 +1200

    Modified layout of my account page edit feature

--- spca-web/src/pages/my-account-page.js
+++ spca-web/src/pages/my-account-page.js
@@ -15,50 +15,57 @@ function MyAccountPage() {
                 <div class="my-account-two-columns-grid">
                     <div className="my-account-col-one">
                         <div>
-                        <div class="my-account-profile-container">
-                            <img className="profile-img" src={require("../images/my-account-icon.png")} alt="Profile Image" />
-                        </div>
-                        <p className="edit-photo-msg"><Link to="/dashboard">Edit</Link> </p>
+                            <div class="my-account-profile-container">
+                                <img className="profile-img" src={require("../images/my-account-icon.png")} alt="Profile Image" />
+                            </div>
+                            <p className="edit-photo-msg"><Link to="/dashboard">Edit</Link> </p>
                         </div>
                     </div>
                     <div className="my-account-col-two">
                         <div class="edit-container">
                             <form>
-                                <div>
-                                    <label for="Name">Name</label>
-                                    {/* <p>Name</p> */}
-                                    <input type="text" id="name" placeholder="Name" />
-                                    <label className="edit-link">Edit</label>
-                                </div>
+                                <div className="user-details-grid">
+                                    <div><label for="Name">Name</label></div>
+                                    <div>
+                                        <p>Name</p>
+                                        {/* <input type="text" id="name" placeholder="Name" /> */}
+                                    </div>
+                                    <div className="edit-align-right"><p><Link to="/dashboard">Edit</Link> </p></div>
 
-                                <div> 
-                                    <label for="ID">ID</label>
-                                    {/* <p>ID</p> */}
-                                    <input type="text" id="id" placeholder="Id" />
-                                </div>
+                                    <div><label for="ID">ID</label></div>
+                                    <div>
+                                        <p>ID</p>
+                                        {/* <input type="text" id="id" placeholder="Id" /> */}
+                                    </div>
+                                    <div></div>
 
-                                <div>
-                                    <label for="Email">Email</label>
-                                    {/* <p>Email</p> */}
-                                    <input type="text" id="email" placeholder="Email" />
-                                </div>
+                                    <div><label for="Email">Email</label></div>
+                                    <div>
+                                        <p>Email</p>
+                                        {/* <input type="text" id="email" placeholder="Email" /> */}
+                                    </div>
+                                    <div className="edit-align-right"><p><Link to="/dashboard">Edit</Link> </p></div>
 
-                                <div>
-                                    <label for="Password">Password</label>
-                                    {/* <p>Password</p> */}
-                                    <input type="password" id="password" placeholder="Password" />
-                                </div>
+                                    <div><label for="Password">Password</label></div>
+                                    <div>
+                                        <p>Password</p>
+                                        {/* <input type="password" id="password" placeholder="Password" /> */}
+                                    </div>
+                                    <div className="edit-align-right"><p><Link to="/dashboard">Edit</Link> </p></div>
 
-                                <div>
-                                    <label for="Role">Role</label>
-                                    {/* <p>Role</p> */}
-                                    <input type="role" id="role" placeholder="Role" />
-                                </div>
+                                    <div><label for="Role">Role</label></div>
+                                    <div>
+                                        <p>Role</p>
+                                        {/* <input type="role" id="role" placeholder="Role" /> */}
+                                    </div>
+                                    <div></div>
 
-                                <div>
-                                    <label for="Joined">Joined</label>
-                                    {/* <p>Joined</p> */}
-                                    <input type="joined" id="joined" placeholder="Joined" />
+                                    <div><label for="Joined">Joined</label></div>
+                                    <div>
+                                        <p>Joined</p>
+                                        {/* <input type="joined" id="joined" placeholder="Joined" /> */}
+                                    </div>
+                                    <div></div>
                                 </div>
                             </form>
                         </div>
