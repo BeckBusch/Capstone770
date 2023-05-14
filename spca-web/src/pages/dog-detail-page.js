62037e568b1f5cf58beefb139acac1c1e2c7f374
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-15 03:26:09 +1200

    Adjusted incorrect broken layout of dog detail page

--- spca-web/src/pages/dog-detail-page.js
+++ spca-web/src/pages/dog-detail-page.js
@@ -6,76 +6,65 @@ import "./dog-detail-page.css";
 function DogDetailPage() {
     return (
         <div className="dog-detail-page">
-            <div className="dog-detail-container">
-                <NavBar/>
-                <div className="outer-button-container">
-                    <div class="dog-detail-button-container">
-                        <div class="buttons">
-                            <Link to="/add-data">
-                                <button type="submit" id="startWeighingBtn" className="start-weighing-btn">
-                                <img className="start-img" src={require("../images/start-icon.png")} alt="start Image" />Start Weighing</button>
-                            </Link>
-                        </div>
-                    </div> 
-                </div>
-                
-                <div class="two-columns-grid">
-                    <div>
-                        <div class="dog-image-align">
-                            <div class="dog-image-container">
-                                <img className="profile-img" src={require("../images/my-account-icon.png")} alt="Profile Image" />
-                            </div>
-                        </div>
+            <NavBar/>
+            <div className="outer-button-container">
+                <div class="dog-detail-button-container">
+                    <div class="buttons">
+                        <Link to="/add-data">
+                            <button type="submit" id="startWeighingBtn" className="start-weighing-btn">
+                            <img className="start-img" src={require("../images/start-icon.png")} alt="start Image" />Start Weighing</button>
+                        </Link>
                     </div>
+                </div> 
+            </div>
 
+            <div className="container-div">
+            
+            <div class="two-columns-grid">
+                <div className="detail-col-one">
                     <div>
-                        <div class="dog-info-container">
-                            <h1 class="name-header">Name</h1>
-                            <div class="two-columns-grid2">
-                                <div class="column1">
-                                    <div>
-                                        <label for="Breed">Breed:</label>
-                                        <p>Maltese</p>
-                                    </div>
-                                    <div>
-                                        <label for="Age">Age:</label>
-                                        <p>3 Years Old</p>
-                                    </div>
-                                    <div>
-                                        <label for="Gender">Gender:</label>
-                                        <p>Male</p>
-                                    </div>
-                                    <div>
-                                        <label for="Location">Location:</label>
-                                        <p>Auckland</p>
-                                    </div>
-                                </div>
-                                <div class="column2">
-                                    <div>
-                                        <label for="Id">ID:</label>
-                                        <p>123456</p>
-                                    </div>
-                                    <div>
-                                        <label for="last-weight">Last Weight:</label>
-                                        <p>7.00kg</p>
-                                    </div>
-                                    <div>
-                                        <label for="weighed-on">Weighed on:</label>
-                                        <p>03/03/2023</p>
-                                    </div>
-                                    <div>
-                                        <label for="weighed-by">Weighed by:</label>
-                                        <p>Juwon Jung </p>
-                                        <p>(Vet)</p>
-                                    </div>
-
-                                </div>
-
-                            </div>
-                            
+                        <div class="dog-image-container">
+                            <img className="profile-img" src={require("../images/my-account-icon.png")} alt="Profile Image" />
                         </div>
                     </div>
                 </div>
+
+                <div className="detail-col-two">
+                    <div class="dog-info-container">
+                        <h1 class="name-header">Name</h1>
+                        <div class="four-columns-grid">
+                            <div><label for="Breed">Breed:</label></div>
+                            <div><p>Maltese</p></div>
+
+                            <div><label for="Id">ID:</label></div>
+                            <div><p>123456</p></div>
+
+
+                            <div><label for="Age">Age:</label></div>
+                            <div><p>3 Years Old</p></div>
+
+                            <div><label for="last-weight">Last Weight:</label></div>
+                            <div><p>7.00kg</p></div>
+
+
+                            <div><label for="Gender">Gender:</label></div>
+                            <div><p>Male</p></div>
+
+                            <div><label for="weighed-on">Weighed on:</label></div>
+                            <div><p>03/03/2023</p></div>
+
+
+                            <div><label for="Location">Location:</label></div>
+                            <div><p>Auckland</p></div>
+
+                            <div><label for="weighed-by">Weighed by:</label></div>
+                            <div><p>Juwon Jung (Vet)</p></div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+
             </div>
 
 
@@ -97,31 +86,17 @@ function DogDetailPage() {
                             </Link>
                         </div>
                     </div>
-
-                    {/* <div className="graph-button-container">
-                            <div class="graph-buttons">
-                                <Link to="/">
-                                    <button type="submit" id="tableBtn" className="table-btn">Table</button>
-                                </Link>
-                                <Link to="/">
-                                    <button type="submit" id="chartBtn" className="chart-btn">Chart</button>
-                                </Link>
-                            </div>
-                    </div> */}
                 
                     <div className="graph">
                         <p>diagram goes here</p>
                         
                     </div>
-
                 </div>
 
                 <div className="extra-msg">
                     <p>** Hover over points to see details - precise measurements, date and time of measurement, vet/volunteer</p>
                 </div>
                 
-                
-
             </div>
 
             
