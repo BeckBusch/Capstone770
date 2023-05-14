03278d28eb810f1a53a8efad4264aef588e32ae8
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-15 03:24:46 +1200

    Modified layout and aligment of sign up page

--- spca-web/src/pages/sign-up-page.js
+++ spca-web/src/pages/sign-up-page.js
@@ -13,50 +13,44 @@ function SignUpPage() {
             </div>    
             <div className="align-center">
                 <div className="sign-up-container">
-                    {/* <img className="spca-logo" src={require("../images/spca-logo.png")} alt="SPCA Logo" /> */}
-                    <h1>Sign Up</h1>
+                    <div className="sign-up-header-container">
+                        <img src={require("../images/my-account-icon-black.png")} className="sign-up-icon" alt="sign-up" />
+                        <h1 className="sign-up-header">Sign Up</h1>
+                    </div>
                     <p>An admin will review and approve your sign up request.</p>
                     <p>Completing this sign up form does not automatically create your account.</p>
-                    <div class="two-columns-grid">
-                        <div>
+                    <div class="sign-two-columns-grid">
+                        <div class="sign-up-col-1">
                             <div class="profile-container">
                                 <img className="profile-img" src={require("../images/my-account-icon.png")} alt="Profile Image" />
                             </div>
                             <p className="add-photo-msg"><Link to="/dashboard">+ Add Photo</Link> </p>
                         </div>
-                        <div>
+                        <div class="sign-up-col-2">
                             <form>
-                                <div>
-                                    <label for="Name">Name</label>
-                                    <input type="text" id="name" placeholder="Name" />
-                                </div>
+                            <div class="sign-up-two-columns-grid">
+                                <div><label for="Name">Name</label></div>
+                                <div><input class="input-styling" type="text" id="name" placeholder="Name" /></div>
 
-                                <div>
-                                    <label for="Email">Email</label>
-                                    <input type="text" id="email" placeholder="Email" />
-                                </div>
+                                <div><label for="Email">Email</label></div>
+                                <div><input class="input-styling" type="text" id="email" placeholder="Email" /></div>
 
-                                <div>
-                                    <label for="Password">Password</label>
-                                    <input type="password" id="password" placeholder="Password" />
-                                </div>
+                                <div><label for="Password">Password</label></div>
+                                <div><input class="input-styling" type="password" id="password" placeholder="Password" /></div>
 
-                                <div>
-                                    <label for="ConfirmPassword">Confirm Password</label>
-                                    <input type="password" id="confirmPassword" placeholder="Confirm Password" />
-                                </div>
+                                <div><label for="ConfirmPassword">Confirm Password</label></div>
+                                <div><input class="input-styling" type="password" id="confirmPassword" placeholder="Confirm Password" /></div>
 
-                                <div>
-                                    <label for="Role">Role</label>
-                                    <div class="dropdown">
-                                        <button class="dropbtn"><i class="arrow down"></i></button>
-                                        <div class="dropdown-content">
-                                            <a href="#">Admin</a>
-                                            <a href="#">Staff</a>
-                                            <a href="#">Volunteer</a>
-                                        </div>
-                                    </div> 
-                                </div>     
+                                <div><label for="Role">Role</label></div>
+                                <div class="role-dropdown">
+                                    <button class="role-dropbtn"><i class="role-arrow down"></i></button>
+                                    <div class="role-dropdown-content">
+                                        <a href="#">Admin</a>
+                                        <a href="#">Staff</a>
+                                        <a href="#">Volunteer</a>
+                                    </div>
+                                </div> 
+                            </div>
                             </form>
                         </div>
                     </div>
@@ -70,7 +64,6 @@ function SignUpPage() {
                             </Link>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
