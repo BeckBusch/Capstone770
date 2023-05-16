a961cc3b8929a90a0c6b2959c8beb9709d0f14d0
sm43576 <79734318+sm43576@users.noreply.github.com>
2023-05-16 13:24:43 +1200

    adjusted adc for 1.65-0.24V

--- project/Embedded Systems/Pico Project/adc.c
+++ project/Embedded Systems/Pico Project/adc.c
@@ -11,20 +11,6 @@
 
 
 double adcConvert() {
-    //Notes 
-    //ADC accepts voltages from 0 -3.3, and is 12 bit, 2^12 = 4096
-    //Each voltage count =3.3/2^12, vCount = 8.05e-4 V per adc count.
-
-    //In practices we want 0-25kg with steps of 0.25 kg as per project proposal,100 counts
-    //
-    //scale factor = 4096/100 = 40.96 counts per 0.25kg or 163.84 counts per 1kg.
-
-
-    //eg 4.25kg dog,
-    // 0.033V vs per 0.25kg 
-    // 0.033 * (4.25/0.25)= 0.561 V sent from analogue to pin
-    //  0.561/ vCount=696 adc count.
-    // 696/scalefactor = 4.248kg = close enough?
 
   /*
         Round 2 Notes
@@ -40,19 +26,41 @@ double adcConvert() {
         the equation would then become 
         conversion = reading/scalefactor - offset.
 
-
         Given that the voltage does not exceed 1.89V
-
     
     */
 
-    int reading = adc_read(); // read ADC value
-    double scaleFactor = 83.88;
-    double offSet = 2.956;
+     /*
+        Round 3 Notes
+        ADC range is 0-3.3V, 12 bit.
+        This means that each count is 8.0566404625e-4V
+
+        Because our range is from 1.65v to 0.24v(0-25kg) this limits our adc count from
+        2048-298,           Voltage/voltage per count = count.
+        ADC range is now 1750 counts and thus, 70 counts per kg
+
+        2048 = 0kg, 1978 = 1kg, 1908 = 2kg
+        Given that the voltage does not exceed the lower or upper bounds
+
+    
+    */
+    double conversion=0;
+    double reading = adc_read(); // read ADC value
+    
+
+    if ((reading>2048) || (reading<298))
+    {
+        printf("faulty reading");
+    }
+
+    else{
+        // equation  = abs(x-2048)/70 = weight
+        conversion = abs(reading - 2048) / 70  ;
+
+
+    }
     
     printf("adc reading check: %f\n", reading); //PRINTER
-
-    double conversion = ((double)reading / scaleFactor)- offSet; //which equates to 5kg
 
     return conversion;
 }
