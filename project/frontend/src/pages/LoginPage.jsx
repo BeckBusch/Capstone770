940db97b8b4a68790045a16f21a84f54ad862dd0
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-16 13:40:57 +1200

    login page done

--- project/frontend/src/pages/LoginPage.jsx
+++ project/frontend/src/pages/LoginPage.jsx
@@ -1,39 +1,48 @@
 import "../css/LoginPage.css";
 import { Link } from "react-router-dom";
-import SPCALogo from "../assets/spca-logo.png"
-import BackgroundImage from "../assets/login-image.png"
+import SPCALogo from "../assets/spca-logo.png";
+import BackgroundImage from "../assets/login-image.png";
 
 function LoginPage() {
   return (
     <div className="login-page">
+      {/* Background Image */}
+      <img className="background-image" src={BackgroundImage} alt="SPCA Logo" />
 
-
+      {/* Login Container */}
       <div className="align-right">
-
         <div className="login-container">
           <img className="spca-logo" src={SPCALogo} alt="SPCA Logo" />
           <form>
             <div>
-              <input type="text" placeholder="Username" className="login-input" />
+              <input
+                type="text"
+                placeholder="Username"
+                className="login-input"
+              />
             </div>
             <div>
-              <input type="password" placeholder="Password" className="login-input" />
+              <input
+                type="password"
+                placeholder="Password"
+                className="login-input"
+              />
             </div>
             <div>
               <Link to="/dashboard">
-                <button type="submit" id="logInBtn" className="login-btn">Log In</button>
+                <button type="submit" id="logInBtn" className="login-btn">
+                  Log In
+                </button>
               </Link>
             </div>
           </form>
-          <p className="sign-up-msg"> Don't have an account? <Link to="/sign-up">Sign Up</Link> </p>
+          <p className="sign-up-msg">
+            Don't have an account? <Link to="/sign-up">Sign Up</Link>
+          </p>
         </div>
-
       </div>
-      
-      <img className="background-image" src={BackgroundImage} alt="SPCA Logo" />
-
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
