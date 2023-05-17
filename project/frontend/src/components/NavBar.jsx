04fcc03b6b9ae0de9289f5f8f165bf37831d8dd6
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-17 23:46:34 +1200

    Removed account settings page and replaced with sign out dropdown menu from nav bar

--- project/frontend/src/components/NavBar.jsx
+++ project/frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import "../css/NavBar.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../AppContextProvider";
 
@@ -8,11 +8,36 @@ import DashboardIcon from "../assets/dashboard-icon.png";
 import ChatIcon from "../assets/chat-icon.png";
 import MyAccountIcon from "../assets/my-account-icon.png";
 import ManageUsersIcon from "../assets/settings-icon.png";
+import SignOutIcon from "../assets/exit.png";
+
 
 function NavBar() {
-    const {userRole, setUserRole, userEmail, setUserEmail} = useContext(AppContext);
+    const {userRole, setUserRole, userEmail, setUserEmail, userName, setUserName} = useContext(AppContext);
 
     console.log("userrole "  + userRole);
+    console.log("username "  + userName);
+
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        useEffect(() => {
+            document.getElementById("mySignOutDropdown").classList.toggle("showing");
+          });
+    }
+    
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.sign-out-dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('showing')) {
+            openDropdown.classList.remove('showing');
+            }
+        }
+        }
+    }
 
     return (userRole == "Admin") ? (
         <div className="nav-bar">
@@ -45,8 +70,13 @@ function NavBar() {
 
             <div className="col col-right">
                 <Link to="/account-settings/:id">
-                    <button type="submit" id="logInBtn" className="navbar-link">My Account</button>
+                <div className="account-settings-align">
+                    <button type="submit" id="logInBtn" className="navbar-link-account">
+                        <span className="line-1">{userName}</span>
+                        <span className="line-2">{userRole}</span>
+                    </button>
                     <img className="navbar-icon" src={MyAccountIcon} alt="Dashboard" />
+                </div>
                 </Link>
             </div>
 
@@ -73,10 +103,26 @@ function NavBar() {
             </div>
 
             <div className="col col-right">
-                <Link to="/account-settings/:id">
-                    <button type="submit" id="logInBtn" className="navbar-link">My Account</button>
-                    <img className="navbar-icon" src={MyAccountIcon} alt="Dashboard" />
-                </Link>
+                <div className="account-settings-align">
+                    <div className="dropdown">
+                        <button onClick={myFunction()} className="sign-out-dropbtn">
+                            <div className="b-container">
+                                <div>
+                                    <span className="line-1">userName</span>
+                                    <span className="line-2">userRole</span>
+                                </div>
+                                <div className="my-account-icon-align-2"> 
+                                    <img className="navbar-icon" src={MyAccountIcon} alt="Dashboard" /> 
+                                </div>
+                            </div>
+                        </button>
+                        <div id="mySignOutDropdown" className="dropdown-content">
+                            <a href="/*">
+                            <img className="sign-out-icon" src={SignOutIcon} alt="Dashboard" />
+                            Sign Out</a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
