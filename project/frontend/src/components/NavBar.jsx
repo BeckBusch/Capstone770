d6457082b38cbc752ad57adb9f2d4c9144f43191
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-19 02:42:38 +1200

    tidied up navbar css

--- project/frontend/src/components/NavBar.jsx
+++ project/frontend/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import "../css/NavBar.css";
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 import { AppContext } from "../AppContextProvider";
 
 import DashboardLogo from "../assets/dashboard-logo.png";
@@ -10,71 +12,79 @@ import MyAccountIcon from "../assets/my-account-icon.png";
 import ManageUsersIcon from "../assets/settings-icon.png";
 import SignOutIcon from "../assets/exit.png";
 
+
 function NavBar() {
   const { userRole, userName } = useContext(AppContext);
 
   console.log("userrole " + userRole);
   console.log("username " + userName);
 
-  /* When the user clicks on the button,
-    toggle between hiding and showing the dropdown content */
-  function myFunction() {
-    useEffect(() => {
-      document.getElementById("mySignOutDropdown").classList.toggle("showing");
-    });
+  const [signOutPopUpVisible, setSignOutPopUpVisible] = useState(false);
+
+  function handlePopUpDisplay() {
+    if (signOutPopUpVisible) {
+      document.getElementById("mySignOutDropdown").style.display = "none";
+      setSignOutPopUpVisible(false);
+    } else {
+      document.getElementById("mySignOutDropdown").style.display = "block";
+      setSignOutPopUpVisible(true);
+    }
   }
 
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function (event) {
-    if (!event.target.matches(".sign-out-dropbtn")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("showing")) {
-          openDropdown.classList.remove("showing");
-        }
-      }
-    }
-  };
+  const navigate = useNavigate();
+
+  function signOutUser() {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  }
 
   return userRole == "Admin" ? (
     <div className="nav-bar">
-      <div className="col col-left">
+      <div className="col-left">
         <img className="navbar-logo" src={DashboardLogo} alt="SPCA Logo" />
       </div>
 
-      <div className="col col-mid">
+      <div className="col-mid">
         <Link to="/dashboard">
           <button type="submit" id="logInBtn" className="navbar-link-button">
-              <img className="navbar-icon" src={DashboardIcon} alt="Dashboard" />
-                <p className="navbar-link-text">Dashboard</p>
+            <img className="navbar-icon" src={DashboardIcon} alt="Dashboard" />
+            <p className="navbar-link-text">Dashboard</p>
           </button>
         </Link>
       </div>
 
-      <div className="col col-mid">
+      <div className="col-mid">
         <Link to="/chat">
           <button type="submit" id="logInBtn" className="navbar-link-button">
-          <img className="navbar-icon" src={ChatIcon} alt="Dashboard" />
-          <p className="navbar-link-text">Chat</p>
+            <img className="navbar-icon" src={ChatIcon} alt="Dashboard" />
+            <p className="navbar-link-text">Chat</p>
           </button>
         </Link>
       </div>
 
-      <div className="col col-mid">
+      <div className="col-mid">
         <Link to="/manage-users">
           <button type="submit" id="logInBtn" className="navbar-link-button">
-          <img className="navbar-icon" src={ManageUsersIcon} alt="Dashboard" />
-          <p className="navbar-link-text">Manage Users</p>
+            <img
+              className="navbar-icon"
+              src={ManageUsersIcon}
+              alt="Dashboard"
+            />
+            <p className="navbar-link-text">Manage Users</p>
           </button>
         </Link>
       </div>
 
-      <div className="col col-right">
+      <div className="col-right">
         <div className="account-settings-align">
           <div className="dropdown">
-            <button onClick={myFunction()} className="sign-out-dropbtn">
+            <button
+              onClick={() => handlePopUpDisplay()}
+              className="sign-out-dropbtn"
+            >
               <div className="b-container">
                 <div>
                   <span className="line-1">{userName}</span>
@@ -90,48 +100,53 @@ function NavBar() {
               </div>
             </button>
 
-            <div id="mySignOutDropdown" className="dropdown-content">
-              <a href="/">
-                <img
-                  className="sign-out-icon"
-                  src={SignOutIcon}
-                  alt="Dashboard"
-                />
-                Sign Out
-              </a>
-            </div>
+            <button
+              id="mySignOutDropdown"
+              className="sign-out-btn"
+              onClick={() => signOutUser()}
+            >
+              <img
+                className="sign-out-icon"
+                src={SignOutIcon}
+                alt="Dashboard"
+              />
+              Sign Out
+            </button>
           </div>
         </div>
       </div>
     </div>
   ) : (
     <div className="nav-bar">
-      <div className="col col-left">
+      <div className="col-left">
         <img className="navbar-logo" src={DashboardLogo} alt="SPCA Logo" />
       </div>
 
-      <div className="col col-mid">
+      <div className="col-mid">
         <Link to="/dashboard">
           <button type="submit" id="logInBtn" className="navbar-link-button">
-              <img className="navbar-icon" src={DashboardIcon} alt="Dashboard" />
-                <p className="navbar-link-text">Dashboard</p>
+            <img className="navbar-icon" src={DashboardIcon} alt="Dashboard" />
+            <p className="navbar-link-text">Dashboard</p>
           </button>
         </Link>
       </div>
 
-      <div className="col col-mid">
+      <div className="col-mid">
         <Link to="/chat">
           <button type="submit" id="logInBtn" className="navbar-link-button">
-          <img className="navbar-icon" src={ChatIcon} alt="Dashboard" />
-          <p className="navbar-link-text">Chat</p>
+            <img className="navbar-icon" src={ChatIcon} alt="Dashboard" />
+            <p className="navbar-link-text">Chat</p>
           </button>
         </Link>
       </div>
 
-      <div className="col col-right">
+      <div className="col-right">
         <div className="account-settings-align">
           <div className="dropdown">
-            <button onClick={myFunction()} className="sign-out-dropbtn">
+            <button
+              onClick={() => handlePopUpDisplay()}
+              className="sign-out-dropbtn"
+            >
               <div className="b-container">
                 <div>
                   <span className="line-1">{userName}</span>
@@ -146,16 +161,19 @@ function NavBar() {
                 </div>
               </div>
             </button>
-            <div id="mySignOutDropdown" className="dropdown-content">
-              <a href="/*">
-                <img
-                  className="sign-out-icon"
-                  src={SignOutIcon}
-                  alt="Dashboard"
-                />
-                Sign Out
-              </a>
-            </div>
+
+            <button
+              id="mySignOutDropdown"
+              className="sign-out-btn"
+              onClick={() => signOutUser()}
+            >
+              <img
+                className="sign-out-icon"
+                src={SignOutIcon}
+                alt="Dashboard"
+              />
+              Sign Out
+            </button>
           </div>
         </div>
       </div>
