b197d6360be15cf0bd605391c0c88342e5f4114a
minssunkim <79774110+minssunkim@users.noreply.github.com>
2023-05-19 01:31:39 +1200

    tidied up frontend for dashbaord

--- project/frontend/src/pages/DashboardPage.jsx
+++ project/frontend/src/pages/DashboardPage.jsx
@@ -1,56 +1,39 @@
 import "../css/DashboardPage.css";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useContext, useState } from "react";
-// import './App.css'
+import { useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import { AppContext } from "../AppContextProvider";
 
 import NavBar from "../components/NavBar";
 import DashboardCard from "../components/DashboardCard";
 
 function DashboardPage() {
-  const { dogs, dogID, setDogID, getAllDogs, sortDog, sortDog2} = useContext(AppContext);
+  const { dogs, setDogID, getAllDogs, sortDogAToZ, sortDogZToA } =
+    useContext(AppContext);
+
   const [rollCount, setRollCount] = useState(0);
 
   const navigate = useNavigate();
 
-  const handleClick = (str) => {
+  function navigateToAddDog() {
+    navigate("/add-dog");
+  }
+
+  function navigateToDogDetails(str) {
     setDogID(str);
-    console.log("dog id = ", str);
-    navigate("/dog/:id");
+    navigate(`/dog/${str}`);
   }
 
-  const sortAlphabetically = () => {
-    console.log("filter button clicked");
-    // dogs.find().sort({})
-    handleGetDogs();
-  }
-  
-  async function handleGetDogs() {
-    const allDogs = await getAllDogs();
-    console.log("Dog List: ", allDogs[0]);
-  }
-
-  const sortAlphabetically2 = () => {
-    console.log("sort button clicked");
-    console.log("rollCount = ", rollCount);
+  function sortAlphabetically() {
     setRollCount(rollCount + 1);
-    console.log("rollCount = ", rollCount);
-    handleGetSortDogs();
+    handleSortDogs();
   }
 
-  async function handleGetSortDogs() {
-    if ((rollCount % 2) == 0) {
-      console.log("EVEN");
-      const allSortDogs = await sortDog();
-      console.log("Dog: ", dogs);
-      console.log("Dog Sort List: ", allSortDogs);
-    } else {
-      console.log("ODD");
-      const allSortDogs2 = await sortDog2();
-      console.log("Dog2: ", dogs);
-      console.log("Dog Sort List2: ", allSortDogs2);
-    }
-    
+  async function handleSortDogs() {
+    rollCount % 2 == 0 ? await sortDogAToZ() : await sortDogZToA();
+  }
+
+  async function handleGetAllDogs() {
+    await getAllDogs();
   }
 
   return (
@@ -59,9 +42,9 @@ function DashboardPage() {
 
       <div className="search-div">
         <div className="add-dog-container-div">
-          <Link to="/add-dog">
-            <button className="add-dog-btn">+ Add Dog</button>
-          </Link>
+          <button className="add-dog-btn" onClick={() => navigateToAddDog()}>
+            + Add Dog
+          </button>
         </div>
         <div className="search-container-div">
           <input
@@ -72,12 +55,13 @@ function DashboardPage() {
           <button className="dashboard-search-button"></button>
         </div>
         <div className="filter-container-div">
-          <button 
-          className="sort-btn"
-          onClick={() => sortAlphabetically2()}
+          <button
+            className="sort-btn"
+            onClick={() => sortAlphabetically()}
           ></button>
-          <button className="filter-btn"
-          onClick={() => sortAlphabetically()}
+          <button
+            className="filter-btn"
+            onClick={() => handleGetAllDogs()}
           ></button>
         </div>
       </div>
@@ -85,22 +69,22 @@ function DashboardPage() {
       <div className="dog-cards-flex">
         {dogs.map(function (dog, i) {
           return (
-            // <Link to="/dog/:id">
-              <button className="dashboard-card-btn"
-              onClick={() => handleClick(dog["_id"])}>
-                <DashboardCard
-                  key={i}
-                  className="dog-card"
-                  name={dog["name"]}
-                  breed={dog["breed"]}
-                  age={dog["age"]}
-                />
-              </button>
-            // </Link>
+            <button
+              className="dashboard-card-btn"
+              onClick={() => navigateToDogDetails(dog["_id"])}
+              key={i}
+            >
+              <DashboardCard
+                key={i}
+                className="dog-card"
+                name={dog["name"]}
+                breed={dog["breed"]}
+                age={dog["age"]}
+              />
+            </button>
           );
         })}
       </div>
-      {/* </div> */}
     </div>
   );
 }
