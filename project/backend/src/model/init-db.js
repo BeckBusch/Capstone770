15b76b82a614ac1d97d17e7c29639a7e830cf23d
jjun273 <jjun273@aucklanduni.ac.nz>
2023-05-18 19:41:31 +1200

    Tested with sort database

--- project/backend/src/model/init-db.js
+++ project/backend/src/model/init-db.js
@@ -24,12 +24,58 @@ export async function main() {
     // Disconnect when complete
     // await mongoose.disconnect();
     // console.log('Disconnected from database!');
+    
+    // await sortDatabase();
+    // console.log("Sorted database");
+
 }
 
 async function clearDatabase() {
     const dogsDeleted = await Dog.deleteMany({});
     const usersDeleted = await User.deleteMany({});
     console.log(`Cleared database (removed ${dogsDeleted.deletedCount} dogs and ${usersDeleted.deletedCount} users ).`);
+}
+
+async function sortDatabase() {
+    const dogsDeleted = await Dog.find({}, {_id:0, name:1, breed:1, age:1, gender:1, location:1, currentWeight:1, prevWeights:1}).sort({"age": 1});
+    // const dogsDeleted = await Dog.find({});
+    console.log(`Dog database sorted alphabetically name`, dogsDeleted);
+
+//     const test = await Dog.insertMany([
+//         {
+//         name: "Test",
+//         breed: "breedTest",
+//         age: 9,
+//         gender: "Female",
+//         location: "Christchurch",
+//         currentWeight: ["hello", "hi"],
+//         prevWeights: []
+//         },
+//         {
+//             name: "Test2",
+//             breed: "breedTes2",
+//             age: 10,
+//             gender: "Female",
+//             location: "Christchurch",
+//             currentWeight: ["hello2", "hi2"],
+//             prevWeights: []
+//         }
+
+// ]);
+    // const test = await Dog.insertMany({dogsDeleted});
+    const deleted = await Dog.deleteMany({});
+    console.log(`Cleared database (removed ${deleted.deletedCount} dogs).`);
+    const userDeleted = await Dog.insertMany(dogsDeleted);
+    console.log(`Dog database inserted`, userDeleted);
+
+    
+    // const test = await Dog.insertMany({name :"Max", breed: "Poodle", gender: "male", age: 1, location: "wellington"});
+    // const test = await Dog.update(sort({"name": 1}));
+
+    // console.log("Store results as an array!");
+
+    // const userDeleted = await Dog.insertMany(dogsDeleted);
+    // console.log(`Database replaced`);
 }
 
 // const dogs = [
